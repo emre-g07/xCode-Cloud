105e0f5edaaafef4a94c2c96306ca860a17cab32
emre-g07 <emre-g07@hotmail.com>
2023-02-03 14:42:50 +0300

    fix

---
+++ xCode-Cloud/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  xCode-Cloud
+//
+//  Created by Emre GURSES on 3.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let dataTypesArray = ["Xcode Cloud - Prepare", "Xcode Cloud - Githup", "Xcode Cloud - Settings", "Xcode Cloud - First Build"]
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Cloud World!")
+            List {
+                ForEach(0..<dataTypesArray.count) { index in
+                  Text(dataTypesArray[index])
+                }
+              }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xCode-Cloud/xCode_CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  xCode_CloudApp.swift
+//  xCode-Cloud
+//
+//  Created by Emre GURSES on 3.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct xCode_CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
