5351208e4e9c7c04625b794c5796c129f38566e2
EmreG <emre.gurses@intertech.com.tr>
2023-04-28 20:53:02 +0300

    add ci_scripts_pre_xcodebuild

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,3 +5,15 @@
 #
 #  Created by emregurs on 26.04.2023.
 #  
+
+if [[ -n $CI_PULL_REQUEST_NUMBER && $CI_XCODEBUILD_ACTION = 'archive' ]];
+then
+    echo "Replacing app icon with beta app icon."
+    APP_ICON_PATH=$CI_WORKSHPACE/Shared/Assets.xcassets/AppIcon.appiconset
+
+    # Remove existing app icon
+    rm -rf $APP_ICON_PATH
+
+    # Add beta app icon
+    mv "$CI_WORKSPACE/ci_scripts/AppIcon-Beta.appiconset" $APP_ICON_PATH
+fi
