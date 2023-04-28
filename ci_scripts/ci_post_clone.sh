8da1d00ac4b0f6df679223b99aea73ea9b98ad37
EmreG <emre.gurses@intertech.com.tr>
2023-04-28 13:36:53 +0300

    added

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,3 +5,10 @@
 #
 #  Created by emregurs on 26.04.2023.
 #  
+
+if [[ -n $CI_PULL_REQUEST_NUMBER ]];
+then
+    echo "This build started from a pull request."
+
+    # Perform an action only if the build starts from a pull request.
+fi
